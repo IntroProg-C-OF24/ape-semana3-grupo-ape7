e54bf07e0ea5b217b3b19be5de4b3c37a6272511
Paula Rivas <pcrivas@utpl.edu.ec>
2023-10-31 18:45:38 -0500

    Ejercicio resuelto 10

---
+++ Ejercicio10/src/ejercicio10/Ejercicio10.java
@@ -0,0 +1,23 @@
+package ejercicio10;
+public class Ejercicio10 {
+    public static void main(String[] args) {
+    int var1 = 25;
+    int var2 = 10;
+    int var3 = 100;
+    int var4 = 5;
+    int var5 = 2;
+    boolean val1 = true;
+    boolean val2 = false;
+    boolean op1;
+    boolean op2;
+    boolean resultado = true;
+    op1 = (Math.sqrt (var1))*var2 >= var3;
+    op2 = (var2/var4 >= var5);
+        System.out.println("Respuesta del primer ejercicio es: " + op1);
+        System.out.println("Respuesta del segundo ejercicio es: " + op2);
+        System.out.println("Respuesta total es: " + resultado);
+            
+            
+    }
+    
+}
